b5e1310fac33200993cdd2a1b2c35dc449a2f243
osh07 <hsg0709@naver.com>
2023-11-06 10:50:59 +0900

    day05 연산, 조건

---
+++ programmers_java/src/day04/ex01.java
@@ -0,0 +1,15 @@
+package day04;
+
+public class ex01 {
+
+	//정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+
+	class Solution {
+		
+	    public int solution(int num, int n) {
+	        int answer = 0;
+	        answer = (num%n==0)?1:0;
+	        return answer;
+	    }
+	}
+}
