1ca46226637a07fef0c35fb22d7b078c37fd2c8c
osh07 <hsg0709@naver.com>
2023-11-06 11:08:13 +0900

    day04 연산, 조건문 update

--- programmers_java/src/day04/ex04.java
+++ programmers_java/src/day04/ex04.java
@@ -9,6 +9,23 @@ public class ex04 {
 	"<", "!" : n < m
 	두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
 	*/
-	
+	class Solution {
+	    public int solution(String ineq, String eq, int n, int m) {
+	        boolean result = false;
+
+	        if (ineq.equals(">") && eq.equals("=")) {
+	            result = (n >= m);
+	        } else if (ineq.equals("<") && eq.equals("=")) {
+	            result = (n <= m);
+	        } else if (ineq.equals(">") && eq.equals("!")) {
+	            result = (n > m);
+	        } else if (ineq.equals("<") && eq.equals("!")) {
+	            result = (n < m);
+	        }
+
+	        return result ? 1 : 0;
+	    }
+	}
+
 	
 }
